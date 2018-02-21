b1ad17991032f94a78a65fc3fe9fb8e606dddec6
Lukas Matt <lukas@zauberstuhl.de>
2018-02-21 16:25:45 +0100

    Diaspora; Fix redis and trim schema.sql

--- diaspora/start.sh
+++ diaspora/start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+redis-server &
+
 cd /diaspora \
-  && sed -i "s/USERNAME/$DATABASE/g" schema.sql \
-  && sed -i "s/GUID/$DATABASE/" schema.sql \
+  && sed -i "s/NAME/$DATABASE/g" schema.sql \
   && sed -i "s/PORT/$PORT/g" schema.sql \
-  && sed -i "s/database: diaspora_development/database: $DATABASE/" config/database.yml \
+  && sed -i "s/database: diaspora_production/database: $DATABASE/" config/database.yml \
   && sed -i "s/url: \".*\"/url: \"http:\/\/localhost:$PORT\/\"/" config/diaspora.yml \
-  && bundle exec rails db:migrate \
+  && bundle exec rails db:drop db:create db:migrate \
   && psql -U postgres -h localhost -p 5432 -d $DATABASE < schema.sql \
   && ./script/server
