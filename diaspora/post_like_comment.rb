59a436ebf2380db084e333ed3cd358d856540510
Lukas Matt <lukas@zauberstuhl.de>
2018-02-22 01:02:33 +0100

    Diaspora script post to all aspects

--- diaspora/post_like_comment.rb
+++ diaspora/post_like_comment.rb
@@ -7,12 +7,13 @@ publicPost = StatusMessageCreationService.new(user).create(
   public: true
 )
 
+aspectIds = Aspect.find_each.collect {|obj| obj.id if obj.user_id == 1 }
 privatePost = StatusMessageCreationService.new(user).create(
   status_message: {
     text: "StatusMessageCreationService privatePost"
   },
   public: false,
-  aspect_ids: [1]
+  aspect_ids: aspectIds
 )
 
 CommentService.new(user).create(publicPost.id, "publicComment")
