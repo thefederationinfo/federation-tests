157bf73e7488edb1b4b789b5a29090a6a83d4024
Lukas Matt <lukas@zauberstuhl.de>
2018-02-23 18:45:16 +0100

    testing_diaspora; Delete static image and bump to v1.0.4-diaspora

--- diaspora/image/README.md
+++
@@ -1,7 +0,0 @@
-# Building
-
-Requires a local postgres database then run:
-
-```
-docker build --network=host -t thefederation/diaspora_image:<tag> .
-```
--- diaspora/share_with.rb
+++
@@ -1,7 +0,0 @@
-user = User.find_by(username: ARGV[0])
-person = Person.find_or_fetch_by_identifier(ARGV[1])
-Aspect.find_each {|obj|
-  if user.id == obj.user_id
-    user.share_with(person, obj)
-  end
-}
