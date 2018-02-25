f92915ecb3038ea8840c9894ae2ccaf32b83c6c5
Lukas Matt <lukas@zauberstuhl.de>
2018-02-26 00:48:45 +0100

    testing_ganggo; Implement PRREPO and PRSHA
    GangGo can therefore use the github integration
    see https://github.com/thefederationinfo/github-integration

--- ganggo/start.sh
+++ ganggo/start.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-cd /ganggo \
-  && sed -i "s/NAME/$DATABASE/g" app.conf \
-  && sed -i "s/PORT/$PORT/g" app.conf \
-  && /ganggo/${BIN} -channel disable -mode dev
+repo=$GOPATH/src/github.com/ganggo/ganggo
+if [ ! -z ${PRSHA} ]; then
+  user=$(echo $PRREPO |cut -d'/' -f1)
+  userrepo=$(echo $PRREPO |cut -d'/' -f2)
+  wd=$repo
+  if [ "$userrepo" -eq "federation" ]; then
+    wd=$repo/vendor/github.com/ganggo/federation
+  fi
+  cd $wd \
+    && git remote add custom https://github.com/$user/$userrepo.git \
+    && git fetch custom \
+    && git checkout $PRSHA \
+    && git log -1
+fi
+
+sed -i "s/NAME/$DATABASE/g" $repo/conf/app.conf \
+  && sed -i "s/PORT/$PORT/g" $repo/conf/app.conf \
+  && revel run github.com/ganggo/ganggo
