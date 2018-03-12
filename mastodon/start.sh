cfd9f6155943d4bbd303fd2dc9d18ea692705230
Lukas Matt <lukas@zauberstuhl.de>
2018-03-12 21:42:57 +0100

    testing_mastodon; Use Sidekiq::Testing.inline
    redis received all jobs butdidn't execute them.

--- mastodon/start.sh
+++ mastodon/start.sh
@@ -2,12 +2,7 @@
 
 WEBPACK_PORT=$(($PORT+100))
 
-# XXX workaround for ruby goldfinger gem
-# adds the ability to search profiles via http
-# (see https://github.com/tootsuite/goldfinger/pull/2)
-sed -i 's/https/http/' \
-  /usr/local/bundle/gems/goldfinger-*/lib/goldfinger/client.rb
-
+sed -i "s/APP_PORT/$PORT/" /mastodon/.env
 sed -i "s/DATABASE_NAME/$DATABASE/" /mastodon/.env
 sed -i "s/port: 3035/port: $WEBPACK_PORT/" config/webpacker.yml
 ./bin/webpack-dev-server --port $WEBPACK_PORT &
