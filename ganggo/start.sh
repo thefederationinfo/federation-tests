1eca0d5e3b3f35a21f960e873f4527d3a12849f8
Lukas Matt <lukas@zauberstuhl.de>
2018-04-03 21:18:38 +0200

    Merge PR's to target branch instead of using PR head
    fixes #14

--- ganggo/start.sh
+++ ganggo/start.sh
@@ -2,26 +2,18 @@
 
 repo=$GOPATH/src/github.com/ganggo/ganggo
 if [ "$PROJECT" == "ganggo" ]; then
-  if [ "$(basename $PRREPO)" == "ganggo.git" ]; then
-    cd $repo && git stash \
-      && git remote add custom $PRREPO \
-      && git fetch custom \
-      && git checkout $PRSHA \
-      && git log -1 || {
-        echo "Cannot find $PRREPO $PRSHA"
-        exit 1
-      }
-  fi
   if [ "$(basename $PRREPO)" == "federation.git" ]; then
-    wd=$repo/vendor/github.com/ganggo/federation
-    rm -r $wd && git clone $PRREPO $wd \
-      && cd $wd \
-      && git checkout $PRSHA \
-      && git log -1 || {
-        echo "Cannot checkout $PRREPO $PRSHA"
-        exit 1
-      }
+    repo=$repo/vendor/github.com/ganggo/federation
   fi
+
+  cd $repo && git stash \
+    && git remote add custom $PRREPO \
+    && git fetch custom \
+    && git merge $PRSHA \
+    && git log -1 || {
+      echo "Cannot find $PRREPO $PRSHA"
+      exit 1
+    }
 fi
 
 sed -i "s/NAME/$DATABASE/g" $repo/conf/app.conf \
