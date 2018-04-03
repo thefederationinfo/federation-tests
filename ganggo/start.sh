3d713b76ac82806b524e768a7b81e95398c081da
Lukas Matt <lukas@zauberstuhl.de>
2018-04-03 21:18:38 +0200

    Merge PR's to target branch instead of using PR head
    ganggo && socialhome
    
    * Install new dependencies and assets on custom build
    * Clone federation vendor repo before merging
    
    fixes #14

--- ganggo/start.sh
+++ ganggo/start.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 repo=$GOPATH/src/github.com/ganggo/ganggo
+# parse and replace configuration values
+sed -i "s/NAME/$DATABASE/g" $repo/conf/app.conf \
+  && sed -i "s/PORT/$PORT/g" $repo/conf/app.conf
+
 if [ "$PROJECT" == "ganggo" ]; then
-  if [ "$(basename $PRREPO)" == "ganggo.git" ]; then
-    cd $repo && git stash \
-      && git remote add custom $PRREPO \
-      && git fetch custom \
-      && git checkout $PRSHA \
-      && git log -1 || {
-        echo "Cannot find $PRREPO $PRSHA"
-        exit 1
-      }
-  fi
   if [ "$(basename $PRREPO)" == "federation.git" ]; then
-    wd=$repo/vendor/github.com/ganggo/federation
-    rm -r $wd && git clone $PRREPO $wd \
-      && cd $wd \
-      && git checkout $PRSHA \
-      && git log -1 || {
-        echo "Cannot checkout $PRREPO $PRSHA"
-        exit 1
-      }
+    repo=$repo/vendor/github.com/ganggo/federation
+    # go-dep prunes git-directory from vendor folder
+    rm -r $repo && git clone \
+      https://github.com/ganggo/federation.git $repo
   fi
+
+  cd $repo && git stash \
+    && git remote add custom $PRREPO \
+    && git fetch custom \
+    && git merge $PRSHA \
+    && git log -1 || {
+      echo "Cannot find $PRREPO $PRSHA"
+      exit 1
+    }
+
+  # install new dependencies
+  # and compile assets
+  make install precompile
 fi
 
-sed -i "s/NAME/$DATABASE/g" $repo/conf/app.conf \
-  && sed -i "s/PORT/$PORT/g" $repo/conf/app.conf \
-  && revel run github.com/ganggo/ganggo
+# start the application server
+revel run github.com/ganggo/ganggo
