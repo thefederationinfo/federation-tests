5047b24aad1e593a4d1f3f6285bb36dd4fd53719
Lukas Matt <lukas@matt.wf>
2018-09-22 18:03:01 +0200

    Use different diaspora redis db's for multi-server use

--- diaspora/start.sh
+++ diaspora/start.sh
@@ -7,6 +7,7 @@ cd /diaspora \
   && sed -i "s/PORT/$PORT/g" schema.sql \
   && sed -i "s/database: diaspora_production/database: $DATABASE/" config/database.yml \
   && sed -i "s/url: \".*\"/url: \"http:\/\/localhost:$PORT\/\"/" config/diaspora.yml \
+  && sed -i "s/#redis:.*/redis: \"redis:\/\/localhost\/${PORT: -1}\"/" config/diaspora.yml \
   && bundle exec rails db:drop db:create db:migrate \
   && psql -U postgres -h localhost -p 5432 -d $DATABASE < schema.sql \
   && ./script/server
